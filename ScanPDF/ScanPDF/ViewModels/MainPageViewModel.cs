12c4528d459f0deff9e6747f90eea54974df94d7
DOLT <dolt@runsystem.net>
2018-12-08 20:47:05 +0700

    init

---
+++ ScanPDF/ScanPDF/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace ScanPDF.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public ICommand SFCommand { get; set; }
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = Resources.TitlePDFScan;
+            TextSF = Resources.Start;
+            SFCommand = new DelegateCommand(HandleActionStartFinish);
+        }
+
+        private string textSF;
+        public string TextSF
+        {
+            get { return textSF; }
+            set { SetProperty(ref textSF, value); }
+        }
+
+        void HandleActionStartFinish()
+        {
+
+        }
+
+    }
+}
